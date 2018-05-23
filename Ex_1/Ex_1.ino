24e8796d5bb8f4f04d0f700ebfa22d4f8f225f78
antonisantiago2017 <37077653+antonisantiago2017@users.noreply.github.com>
2018-05-23 21:12:39 +0200

    Add files via upload

---
+++ Ex_1/Ex_1.ino
@@ -0,0 +1,28 @@
+/**********************************************************************************
+**                                                                               **
+**                                                                               **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+
+//********** Setup ****************************************************************
+void setup()                // inicia una vegada, quan arrenca
+{  Serial.begin(9600);
+Serial.println("Escull el numero de l'operacio que vols realitzar");
+Serial.println("  1. Comprovar numero de tarjeta de credit");
+Serial.println("  2. Comprovar numero de compte bancari");
+Serial.println("  3. Buscar un digit perdut de tarjeta de credit");
+
+}
+
+//********** Loop *****************************************************************
+void loop()                 //s'executa en loop infinit
+{
+
+}
+
+//********** Funcions ************************************************
